01e9e506b4f55628502d789b0a74387a74b80f52
RishabhRawatAndroid <rishicentury1996@gmail.com>
2019-03-20 23:57:06 +0530

    Fix the Airthmetic Divide by 0 Error

--- app/src/main/java/com/myapps/rishabhrawat/codingebooks/generalclasses/AdViewBehaviour.java
+++ app/src/main/java/com/myapps/rishabhrawat/codingebooks/generalclasses/AdViewBehaviour.java
@@ -3,6 +3,7 @@ package com.myapps.rishabhrawat.codingebooks.generalclasses;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewCompat;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.ads.AdView;
@@ -19,8 +20,7 @@ public class AdViewBehaviour extends CoordinatorLayout.Behavior<AdView> {
     public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
                                        AdView child, @NonNull
                                                View directTargetChild, @NonNull View target,
-                                       int axes, int type)
-    {
+                                       int axes, int type) {
         return axes == ViewCompat.SCROLL_AXIS_VERTICAL;
     }
 
@@ -28,8 +28,11 @@ public class AdViewBehaviour extends CoordinatorLayout.Behavior<AdView> {
     public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull AdView child,
                                @NonNull View target, int dxConsumed, int dyConsumed,
                                int dxUnconsumed, int dyUnconsumed,
-                               @ViewCompat.NestedScrollType int type)
-    {
-        child.setTranslationY(Math.max(0f, Math.min(Float.parseFloat(String.valueOf(child.getHeight()/child.getHeight())),child.getTranslationY()+dyConsumed/2)));
+                               @ViewCompat.NestedScrollType int type) {
+        try {
+            child.setTranslationY(Math.max(0f, Math.min(Float.parseFloat(String.valueOf(child.getHeight() / child.getHeight())), child.getTranslationY() + dyConsumed / 2)));
+        } catch (Exception ex) {
+            Log.d("Rishabh", "ERROR COME " + ex);
+        }
     }
 }
